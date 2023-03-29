8e976ffb3ecbd311946609fc01488721cf095c95
Dallen Baldwin <dallen.baldwin@horrocks.com>
2023-03-29 15:14:38 -0600

    remove stuff for now

--- database-normalization-typeorm/src/index.ts
+++ database-normalization-typeorm/src/index.ts
@@ -1,17 +1,7 @@
 import { AppDataSource } from './data-source';
 
 AppDataSource.initialize()
-  .then(async ({ entityMetadatas }) => {
-    entityMetadatas.forEach(({ name, columns, foreignKeys }) =>
-      console.log({
-        name,
-        foreignKeys,
-        columns: columns.map(({ databaseName, referencedColumn, type }) => ({
-          databaseName,
-          referencedColumn,
-          type,
-        })),
-      })
-    );
-  })
+  .then(async ({ entityMetadatas }) =>
+    console.log(entityMetadatas.map(({ name }) => name))
+  )
   .catch(error => console.log(error));
