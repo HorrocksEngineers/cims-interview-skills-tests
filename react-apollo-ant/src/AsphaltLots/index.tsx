8c47e64699d13fe59bbaebe50b63365cf4c54c16
brysonm1 <brysonmay15@hotmail.com>
2023-04-27 14:36:34 -0600

    Initial commit

--- react-apollo-ant/src/AsphaltLots/index.tsx
+++ react-apollo-ant/src/AsphaltLots/index.tsx
@@ -1,4 +1,7 @@
 import { Typography } from 'antd';
+import useProjectAsphaltLots from './query'
+import { Space, Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 
 interface AsphaltLotsProps {}
 
@@ -6,9 +9,17 @@ interface AsphaltLotsProps {}
  * TODO communicate the loading state of the query
  */
 const AsphaltLots: React.FC<AsphaltLotsProps> = () => {
+  let data = useProjectAsphaltLots()
+  let lots = []
+
+  if (data.data) {
+    lots = data.data.project.asphaltLots
+  }
+
   return (
     <>
       <Typography.Title>Asphalt Lots</Typography.Title>
+      {}
     </>
   );
 };
